a51b1808a0a6757dfa6f6b4fcc39ce3875f65215
Henry <128566178+SaintDig1t@users.noreply.github.com>
2024-01-30 13:04:06 -0400

    Class 00
    These are notes of class 00 of the intro to programming course of inter bayamon

---
+++ class_00.cpp
@@ -0,0 +1,28 @@
+//=================================================================================================================================
+// Jan 25, hello world assignment class 
+// Name: Henry M. Rivera Osso
+// Student Number: Y00575252
+// Description: Hello world C++
+//=================================================================================================================================
+#include <iostream> // <- preprocessor directive 
+using namespace std; // <- which namespace to use 
+
+int main(void) // <- beggining of functions named main
+{ // <- main block begins  
+    cout << "!!! Hello World !!! \n" ; // < - output statement 
+    cout << "!!! Hello bro !!! \n" ;
+    return 0; // <- send 0 to operating system 
+} // <- main block ends 
+
+
+
+// Professor uses iostream not cstdio
+
+// There must always be a function called main ()
+// Executim always begins with the first statement in function 
+// Any other function in your program are subprograms and are not executed until they are called
+// main function is always an integer 
+// cout is the only line of program that causes anything to be printed on screen 
+// a program is a set of instructions for the computer 
+//semicolon is requires to mark the end of a complete statement 
+//return 0 indicates the program was successful
